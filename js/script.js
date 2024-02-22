3ec677a2378ded36a0bd78cdb16f58de17c12075
Mirnes Glamočić <contact@mirnesglamocic.com>
2024-02-22 15:47:33 +0100

    Changed http to https for fancybox

--- js/script.js
+++ js/script.js
@@ -161,7 +161,7 @@ function getOutput(item){
     '<img src="' + thumb + '">' +
     '</div>' +
     '<div class="list-right">' +
-    '<h3><a class="fancybox fancybox.iframe" href="http://www.youtube.com/embed/'+videoId+'">' + title + '</a></h3>' +
+    '<h3><a class="fancybox fancybox.iframe" href="https://youtube.com/embed/'+videoId+'">' + title + '</a></h3>' +
     '<small>By <span class="cTitle">' + channelTitle + '</span> on '+ videoDate + '</small>' +
     '<p>'+ description + '</p>' +
     '</div>' +
